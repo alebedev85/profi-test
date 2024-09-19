a2a268ca2d4f99bd80d8d6fc7c8c2de1eb19bf81
Andrey Lebedev <alebedev424@gmail.com>
2024-09-19 12:30:35 +0300

    feat: сделал компонент Data

--- src/components/ReportData/ReportData.jsx
+++ src/components/ReportData/ReportData.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Button } from "@mui/material";
+import InputIcon from "@mui/icons-material/Input";
+import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
+import CloseIcon from "@mui/icons-material/Close";
 
 import styles from "./ReportData.module.scss";
 
 export default function ReportData() {
   return (
     <div className={styles.data}>
-      <Button>Загрузить данные из csv</Button>
-      <Button>Изменить данные</Button>
-      <Button>Очистить</Button>
+      <div className={styles.left}>
+        <Button startIcon={<InputIcon />} className={styles.button}>
+          Загрузить данные из csv
+        </Button>
+        <Button startIcon={<CreateNewFolderIcon />} className={styles.button}>
+          Изменить данные
+        </Button>
+      </div>
+      <div className={styles.right}>
+        <Button endIcon={<CloseIcon />} className={styles.button}>
+          Очистить
+        </Button>
+      </div>
     </div>
   );
 }
